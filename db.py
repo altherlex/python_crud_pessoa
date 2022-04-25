0ff24c9f82db61003fa13faa160ccadb0aab1807
Alther Alves <para.alves@gmail.com>
2022-04-24 19:34:49 -0700

    create update db helper

--- db.py
+++ db.py
@@ -8,7 +8,16 @@ def select(raw_sql):
   conn = mysql.connect()
   cursor = conn.cursor()    
   cursor.execute(raw_sql)
+  # TODO: Return result as Object instead of a array
   result = cursor.fetchall()
   cursor.close()
   conn.close()
   return result
+
+def insert(raw_sql, values):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(raw_sql, values)
+    conn.commit()
+    cursor.close()
+    conn.close()
